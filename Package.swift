d0d47d280d67894c24bf8471fe130de3d6ae764d
Javier Cicchelli <javier@rock-n-code.com>
2025-01-27 23:38:34 +0000

    Package target setup (#1)
    This PR contains the work done to setup the *executable*, *library*, and *test* targets for the Swift package. In addition, some boilerplate code has been removed.
    
    Reviewed-on: https://repo.rock-n-code.com/rock-n-code/colibri/pulls/1
    Co-authored-by: Javier Cicchelli <javier@rock-n-code.com>
    Co-committed-by: Javier Cicchelli <javier@rock-n-code.com>

--- Package.swift
+++ Package.swift
@@ -1,14 +1,46 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Colibri",
+    products: [
+        .executable(
+            name: "colibri",
+            targets: ["Colibri"]
+        ),
+        .library(
+            name: "ColibriLibrary",
+            targets: ["ColibriLibrary"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-argument-parser",
+            from: "1.0.0"
+        )
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "Colibri"),
+            name: "Colibri",
+            dependencies: [
+                .product(
+                    name: "ArgumentParser",
+                    package: "swift-argument-parser"
+                ),
+                .target(name: "ColibriLibrary")
+            ],
+            path: "Sources/Executable"
+        ),
+        .target(
+            name: "ColibriLibrary",
+            dependencies: [],
+            path: "Sources/Library"
+        ),
+        .testTarget(
+            name: "ColibriTests",
+            dependencies: ["ColibriLibrary"],
+            path: "Tests/Library"
+        )
     ]
 )
---
+++ Sources/Executable/Colibri.swift
@@ -0,0 +1,13 @@
+import ArgumentParser
+import ColibriLibrary
+
+@main
+struct Colibri: AsyncParsableCommand {
+    
+    // MARK: Functions
+    
+    func run() async throws {
+        // ...
+    }
+    
+}
